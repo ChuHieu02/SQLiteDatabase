845357d09de10462372fd51ffca83582f560795b
ChuHieu <chuhieu020297@gmail.com>
2019-06-19 07:57:24 +0700

    insert

--- app/src/main/java/com/chuhieu/sqlite_database/ListNguoidungActivity.java
+++ app/src/main/java/com/chuhieu/sqlite_database/ListNguoidungActivity.java
@@ -9,5 +9,6 @@ public class ListNguoidungActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_nguoidung);
+//        To day is the beautiful
     }
 }
